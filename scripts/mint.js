bffb051bf6f7c05ffd44163f1c93c27f186cf05d
lima <chocgamoon95@gmail.com>
2022-04-18 09:38:13 +0900

    test script

--- scripts/mint.js
+++ scripts/mint.js
@@ -17,6 +17,11 @@ task("set-base-token-uri", "Sets the base token URI for the deployed smart contr
 .setAction(async function (taskArguments, hre) {
     const contract = await getContract("NFT", hre);
     const transactionResponse = await contract.setBaseTokenURI(taskArguments.baseUrl, {
+        // GasLimit은 사용할 가스에 대한 예측치 
+        // GasLimit이 높다면 좋지만 자원상의 이유, Etherium 환경으로 인해서 적당하게 사용
+        // Gas가 필요한 이유는 EVM상에서 트랜잭션을 동작시키기 위해 소모되는 비용
+        // Gas Price : Gas Limit에서 1Gas 당 가격으로 높을수록 빠르게 실행된다.
+
         gasLimit: 500_000,
     });
     console.log(`Transaction Hash: ${transactionResponse.hash}`);
